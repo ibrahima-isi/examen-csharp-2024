130aca7d19463b84022085a1eead797c39d81f9f
Ibrahima Diallo <ibrahimadiallo@groupeisi.com>
2024-07-31 11:54:56 +0000

    removing the db file

--- backend/db.php
+++ backend/db.php
@@ -2,7 +2,7 @@
     $db_name = "csharp_api";
     $db_server = "127.0.0.1";
     $db_user = "root";
-    $db_pass = "passer";
+    $db_pass = "";
     try{
         $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
