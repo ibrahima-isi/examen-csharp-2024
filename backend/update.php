aef48b10551205475f85fabb5383fd2417f24e09
Ibrahima Diallo <ibrahimadiallo@groupeisi.com>
2024-07-31 11:23:57 +0000

    replacing include by include_once

--- backend/update.php
+++ backend/update.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-include "../backend/db.php";
+include_once "../backend/db.php";
 
 // Vérifie la méthode de la requête
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
